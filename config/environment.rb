a8e115bad22d3f4158418eb9d3c94124ce51cc59
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-15 19:50:30 +0000

    made neccesary changes to allow test homepage to load

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ Bundler.require
 
 require_all 'app'
 
-ActiveRecord::Base.establish-connection(
+ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/development.sqlite"
 )
