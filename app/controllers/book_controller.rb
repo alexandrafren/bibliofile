683120bcf03032fc9af1aa585b8c5dd62d5dbd97
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-16 20:26:48 +0000

    add in ability to create a new book through post request

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -8,6 +8,15 @@ class BookController < ApplicationController
     erb :'/books/new'
   end
 
+  post '/books' do
+    @user = current_user
+    @book = Book.find_or_create_by(title: params[:title])
+    @book.author = params[:author]
+    @book.save
+    @user.books << @book
+    @bookrating = BookUser.new(user_id: current_user.id, book_id: @book.id, rating: params[:rating], review: params[:review])
+  end
+
   get '/books/:id/edit' do
   end
 
