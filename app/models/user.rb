2930b6d6d084094507e94d2f15fb609353b02da1
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-16 21:38:14 +0000

    add slug methods to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_many :book_users
   has_many :books, :through => :book_users
   has_secure_password
+
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+end
 end
