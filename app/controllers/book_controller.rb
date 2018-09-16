d3cd38a85ab4babe8094697ccbca5041f7d57ae6
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-16 20:05:45 +0000

    add in books controller and routes as well as created books folder under views

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,17 @@
+class BookController < ApplicationController
+  get '/books' do
+  end
+
+  get '/books/new' do
+  end
+
+  get '/books/:id/edit' do
+  end
+
+  get '/books/:id/delete' do
+  end
+
+  get '/books/:id' do
+  end
+
+end
