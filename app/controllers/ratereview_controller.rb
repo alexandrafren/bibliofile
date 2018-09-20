493b4b7aed441e34229d55deb4eb0a4b28c0a875
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-20 22:13:26 +0000

    add in flash messages for rateview controller

--- app/controllers/ratereview_controller.rb
+++ app/controllers/ratereview_controller.rb
@@ -1,11 +1,18 @@
+require 'rack-flash'
 class RatereviewController < ApplicationController
+  use Rack::Flash
   #paths for updating a specific users reviews and ratings
   get '/books/:slug/edit' do
     @book = Book.find_by_slug(params[:slug])
     @review = Review.find_by(user_id: current_user, book_id: @book.id)
     @rating = Rating.find_by(user_id: current_user, book_id: @book.id)
-    if logged_in?
+    if logged_in? && current_user.books.include?(@book)
       erb :'/books/edit'
+    elsif !logged_in?
+      flash[:message] = "You must be logged in to edit a book."
+      redirect to '/login'
+    elsif current_user.books.include?(@book) == nil
+      flash[:message] = "You do not currently have this book on your shelf."
     end
   end
 
@@ -19,6 +26,9 @@ class RatereviewController < ApplicationController
       @rating.value = params[:rating]
       @rating.save
       redirect to '/books'
+    else
+      flash[:message] = "Your input was invalid. Please try again."
+      redirect to '/books'
     end
   end
 
@@ -31,6 +41,11 @@ class RatereviewController < ApplicationController
       @rating = Review.find_by(user_id: current_user, book_id: @book.id)
       @review.destroy
       @rating.destroy
+    elsif !logged_in?
+      flash[:message] = "You must be logged in to delete a book from your shelf."
+      redirect to '/login'
+    elsif current_user.books.include?(@book) == nil
+      flash[:message] = "You do not have this book saved to your shelf. You can't delete something that doesn't exist!"
     end
     redirect to '/books'
   end
