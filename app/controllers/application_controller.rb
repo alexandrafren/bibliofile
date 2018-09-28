eebbaed6d42ff4a2fa31ac4aaccf2b5a08b20d58
Alexandra Fren <alexandra.fren@gmail.com>
2018-09-28 15:36:12 +0000

    create new helper method redirect_if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,11 @@ class ApplicationController < Sinatra::Base
     !!session[:user_id]
   end
 
-end
+  def redirect_if_not_logged_in
+    if !logged_in?
+      flash[:message] = "You must be logged in to see that page!"
+      redirect to '/login'
+    end
+  end
 
 end
